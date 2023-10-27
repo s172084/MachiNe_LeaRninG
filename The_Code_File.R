c33846813296568464c840993c86214715257a4e
PietroLombardo <10669058@polimi.it>
2023-10-27 17:57:49 +0200

    K-fold for classification

--- The_Code_File.R
+++ The_Code_File.R
@@ -57,9 +57,16 @@ summary(diamonds$cut == 'Ideal')
 (percentage_of_Ideal <- sum(diamonds$cut == 'Ideal')/length(diamonds$cut))
 
 X <- as.data.frame(diamonds[,c(1,3:10)])
-X <- X[,-2:3] # Remove Color and Clarity, prediction based on quantitative attributes
+X <- X[,-(2:3)] # Remove Color and Clarity, prediction based on quantitative attributes
 head(X)
 y <- as.numeric(diamonds$cut == 'Ideal')
+N <- dim(X)[1]
+M <- dim(X)[2]
+attributeNames <- colnames(X)
+
+################################
+# Holdout method
+################################
 
 # 8.1.2
 # ----------------
@@ -70,17 +77,17 @@ y <- as.numeric(diamonds$cut == 'Ideal')
 set.seed(5142)
 
 CV <- list()
-CV$which <- createFolds(y, k = 10, list = F)
+CV$which <- createFolds(y, k = 20, list = F)
 CV$TrainSize <- c()
 CV$TestSize <- c()
 
 # Extract the training and test set
-X_train <- X[CV$which == 1, ]
+X_train <- X[CV$which != 1, ]
 # Train on 1/20 of the data, or 5 percent
-y_train <- y[CV$which == 1]
-X_test <- X[CV$which != 1, ]
+y_train <- y[CV$which != 1]
+X_test <- X[CV$which == 1, ]
 # Test on the rest
-y_test <- y[CV$which != 1]
+y_test <- y[CV$which == 1]
 CV$TrainSize[1] <- length(y_train)
 CV$TestSize[1] <- length(y_test)
 
@@ -186,34 +193,204 @@ par(cex.axis = 1) # Define size of axis labels
 }
 
 ################################
-# -------  Question 1 ---------
+# K-folds cross-validation
 ################################
 
+# -----------------------------
+# Regularized Linear regression
+# -----------------------------
+
+# Cross-validation
+
+# Create cross-validation partition for evaluation of performance of optimal model
+K <- 5
+
+# Set seed for reproducibility
+set.seed(4321)
+
+CV <- list()
+CV$which <- createFolds(y, k = K, list = F)
+
+# Set up vectors that will store sizes of training and test sizes
+CV$TrainSize <- c()
+CV$TestSize <- c()
+
+# Values of lambda
+lambda_tmp <- 10^(-5:8)
+
+# Initialize variables
+
+KK <- 10 # nr. of Inner loops
+T <- 14 # nr. of tested lambda
+temp <- rep(NA, M * T * KK)
+w <- array(temp, c(M, T, KK))
+Error_train2 <- matrix(rep(NA, times = T * KK), nrow = T)
+Error_test2 <- matrix(rep(NA, times = T * KK), nrow = T)
+lambda_opt <- rep(NA, K)
+w_rlr <- matrix(rep(NA, times = M * K), nrow = M)
+Error_train_rlr <- rep(NA, K)
+Error_test_rlr <- rep(NA, K)
+w_noreg <- matrix(rep(NA, times = M * K), nrow = M)
+mu <- matrix(rep(NA, times = M * K), nrow = K)
+sigma <- matrix(rep(NA, times = M * K), nrow = K)
+Error_train <- rep(NA, K)
+Error_test <- rep(NA, K)
+Error_train_nofeatures <- rep(NA, K)
+Error_test_nofeatures <- rep(NA, K)
 
 
+for (k in 1:K) {
+  paste("Crossvalidation fold ", k, "/", K, sep = "")
+  
+  # Extract the training and test set
+  X_train <- X[CV$which != k, ]
+  y_train <- y[CV$which != k]
+  X_test <- X[CV$which == k, ]
+  y_test <- y[CV$which == k]
+  CV$TrainSize[k] <- length(y_train)
+  CV$TestSize[k] <- length(y_test)
+  
+  # Use 10-fold cross-validation to estimate optimal value of lambda
+  KK <- 10
+  
+  CV2 <- list()
+  CV2$which <- createFolds(y_train, k = KK, list = F)
+  CV2$TrainSize <- c()
+  CV2$TestSize <- c()
+  
+  
+  for (kk in 1:KK) {
+    X_train2 <- X_train[CV2$which != kk, ]
+    y_train2 <- y_train[CV2$which != kk]
+    X_test2 <- X_train[CV2$which == kk, ]
+    y_test2 <- y_train[CV2$which == kk]
+    
+    mu2 <- colMeans(X_train2[,1:M])
+    sigma2 <- apply(X_train2[,1:M], 2, sd)
+    
+    X_train2[,1:M] <- scale(X_train2[,1:M], mu2, sigma2)
+    X_test2[,1:M] <- scale(X_test2[,1:M], mu2, sigma2)
+    
+    CV2$TrainSize[kk] <- length(y_train)
+    CV2$TestSize[kk] <- length(y_test2)
+    
+    mdl <- glmnet(X_train2, y_train2, family = "binomial", alpha = 0,
+                  lambda = lambda_tmp,intercept=T)
+    
+    train_error <- rep(NA, T)
+    test_error <- rep(NA, T)
+    coefficient_norm <- rep(NA, T)
+    for (k in 1:T) {
+      # Predict labels for both sets for current regularization strength
+      y_train_est <- predict(mdl, newx=as.matrix(X_train), type = "class",
+                             s = lambda_tmp[k])
+      y_test_est <- predict(mdl, newx=as.matrix(X_test), type = "class",
+                            s = lambda_tmp[k])
+      
+      # Determine training and test set error
+      train_error[k] <- sum(y_train_est != y_train) / length(y_train)
+      test_error[k] <- sum(y_test_est != y_test) / length(y_test)
+      
+      # Determine betas and calculate norm of parameter vector
+      w_est <- predict(mdl, type = "coef", s = lambda_tmp[k])[-1]
+      coefficient_norm[k] <- sqrt(sum(w_est^2))
+    }
+  }
+  
+  # Select optimal value of lambda
+  min_error <- min(test_error)
+  lambda_opt[k] <- lambda_tmp[which.min(test_error)]
+  
+  # Standardize outer fold based on training set, and save the mean and standard
+  # deviations since they're part of the model (they would be needed for
+  # making new predictions) - for brevity we won't always store these in the scripts
+  mu[k, ] <- colMeans(X_train[,1:M])
+  sigma[k, ] <- apply(X_train[,1:M], 2, sd)
+  
+  X_train[,1:M] <- scale(X_train[,1:M], mu[k, ], sigma[k, ])
+  X_test[,1:M] <- scale(X_test[,1:M], mu[k, ], sigma[k, ])
+  
+  ###
+  # Estimate w for the optimal value of lambda
+  mdl <- glmnet(X_train, y_train, family = "binomial", alpha = 0,
+                lambda = lambda_tmp,intercept=T)
+  
+  y_train_est <- predict(mdl, newx=as.matrix(X_train), type = "class",
+                         s = lambda_opt[k])
+  y_test_est <- predict(mdl, newx=as.matrix(X_test), type = "class",
+                        s = lambda_opt[k])
+  
+  # evaluate training and test error performance for optimal selected value of lambda
+  Error_train_rlr[k] <- sum(y_train_est != y_train) / length(y_train)
+  Error_test_rlr[k] <- sum(y_test_est != y_test) / length(y_test)
+  ###
+  
+  # # Compute squared error without regularization
+  # # Adds a small value to diagonal to avoid a singular matrix
+  # w_noreg[, k] <- solve(XtX + (diag(M) * 1e-10)) %*% Xty
+  # Error_train[k] <- sum((y_train - X_train %*% w_noreg[, k])^2)
+  # Error_test[k] <- sum((y_test - X_test %*% w_noreg[, k])^2)
+  # 
+  # # Compute squared error without using the input data at all
+  # Error_train_nofeatures[k] <- sum((y_train - mean(y_train))^2)
+  # Error_test_nofeatures[k] <- sum((y_test - mean(y_train))^2)
+  
+  # if (k == K) {
+  #   dev.new()
+  #   # Display result for cross-validation fold
+  #   w_mean <- apply(w, c(1, 2), mean)
+  #   
+  #   # Plot weights as a function of the regularization strength (not offset)
+  #   par(mfrow = c(1, 2))
+  #   par(cex.main = 1.5) # Define size of title
+  #   par(cex.lab = 1) # Define size of axis labels
+  #   par(cex.axis = 1) # Define size of axis labels
+  #   par(mar = c(5, 4, 3, 1) + .1) # Increase margin size to allow for larger axis labels
+  #   
+  #   plot(log(lambda_tmp), w_mean[2, ],
+  #        xlab = "log(lambda)",
+  #        ylab = "Coefficient Values", main = paste("Weights, fold ", k, "/", K),
+  #        ylim = c(min(w_mean[-1, ]), max(w_mean[-1, ]))
+  #   )
+  #   lines(log(lambda_tmp), w_mean[2, ])
+  #   
+  #   colors_vector <- colors()[c(1, 50, 26, 59, 101, 126, 151, 551, 71, 257, 506, 634, 639, 383)]
+  #   
+  #   for (i in 3:M) {
+  #     points(log(lambda_tmp), w_mean[i, ], col = rainbow(T)[i])
+  #     lines(log(lambda_tmp), w_mean[i, ], col = rainbow(T)[i])
+  #   }
+  #   
+  #   plot(log(lambda_tmp), log(apply(Error_train2, 1, sum) / sum(CV2$TrainSize)),
+  #        xlab = "log(lambda)", ylab = "log(Error)",
+  #        main = paste0("Optimal lambda: 1e", log10(lambda_opt[k]))
+  #   )
+  #   
+  #   lines(log(lambda_tmp), log(apply(Error_train2, 1, sum) / sum(CV2$TrainSize)))
+  #   points(log(lambda_tmp), log(apply(Error_test2, 1, sum) / sum(CV2$TestSize)), col = "red")
+  #   lines(log(lambda_tmp), log(apply(Error_test2, 1, sum) / sum(CV2$TestSize)), col = "red")
+  #   
+  #   legend("bottomright", legend = c("Training", "Test"), col = c("black", "red"), lty = 1)
+  #   
+  # }
+}
+
+# Display Results
+writeLines("Linear regression without feature selection:")
+writeLines(paste("- Training error: ", sum(Error_train) / sum(CV$TrainSize)))
+writeLines(paste("- Test error", sum(Error_test) / sum(CV$TestSize)))
+writeLines(paste("- R^2 train:     %8.2f\n", (sum(Error_train_nofeatures) - sum(Error_train)) / sum(Error_train_nofeatures)))
+writeLines(paste("- R^2 test:     %8.2f\n", (sum(Error_test_nofeatures) - sum(Error_test)) / sum(Error_test_nofeatures)))
+
+writeLines("Regularized Linear regression:")
+writeLines(paste("- Training error:", sum(Error_train_rlr) / sum(CV$TrainSize)))
+writeLines(paste("- Test error:", sum(Error_test_rlr) / sum(CV$TestSize)))
+writeLines(paste("- R^2 train: ", (sum(Error_train_nofeatures) - sum(Error_train_rlr)) / sum(Error_train_nofeatures)))
+writeLines(paste("- R^2 test:", (sum(Error_test_nofeatures) - sum(Error_test_rlr)) / sum(Error_test_nofeatures)))
 
 
-################################
-# -------  Question 2 ---------
-################################
-
-
-
-
-
-################################
-# -------  Question 3 ---------
-################################
-
-
-################################
-# -------  Question 4 ---------
-################################
-
-
-
-################################
-# -------  Question 5 ---------
-################################
-
+writeLines("Weights in last fold :")
+for (m in 1:M) {
+  writeLines(paste(attributeNames[m], w_rlr[m, k]))
+}
 
